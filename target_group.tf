77b3bd467ac5f489657232f6f7cb438b0f295a12
Sergey Kuzmich <contact@kuzmi.ch>
2021-07-08 10:35:57 +0300

    Change resource naming

--- target_group.tf
+++ target_group.tf
@@ -1,5 +1,5 @@
 resource "aws_alb_target_group" "main" {
-  name        = format("%s-%s-%s", var.name, var.environment, "tg")
+  name        = substr(format("%s-%s-%s", var.name, var.environment, "tg"), 0, 32)
   port        = 80
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
